0805ac864c931881e18206d2eb25424d9d8d3de7
Eric Laurello <eric.laurello@flipsidecrypto.com>
2022-09-27 08:33:57 -0400

    check in

---
+++ models/core/core__fact_set_ip_address_events.sql
@@ -0,0 +1,48 @@
+{{ config(
+  materialized = 'incremental',
+  unique_key = 'fact_set_ip_address_events_id',
+  incremental_strategy = 'merge',
+  cluster_by = ['block_timestamp::DATE']
+) }}
+
+WITH base AS (
+
+  SELECT
+    node_address,
+    ip_addr,
+    event_id,
+    block_timestamp,
+    _INSERTED_TIMESTAMP
+  FROM
+    {{ ref('silver__set_ip_address_events') }}
+
+{% if is_incremental() %}
+WHERE
+  _inserted_timestamp >= (
+    SELECT
+      MAX(
+        _inserted_timestamp
+      )
+    FROM
+      {{ this }}
+  )
+{% endif %}
+)
+SELECT
+  {{ dbt_utils.surrogate_key(
+    ['a.event_id','a.node_address','a.ip_addr','a.block_timestamp']
+  ) }} AS fact_set_ip_address_events_id,
+  b.block_timestamp,
+  COALESCE(
+    b.dim_block_id,
+    '-1'
+  ) AS dim_block_id,
+  node_address,
+  ip_addr,
+  A._INSERTED_TIMESTAMP,
+  '{{ env_var("DBT_CLOUD_RUN_ID", "manual") }}' AS _audit_run_id
+FROM
+  base A
+  LEFT JOIN {{ ref('core__dim_block') }}
+  b
+  ON A.block_timestamp = b.timestamp
---
+++ models/core/core__fact_set_mimir_events.sql
@@ -0,0 +1,48 @@
+{{ config(
+  materialized = 'incremental',
+  unique_key = 'fact_set_mimir_events_id',
+  incremental_strategy = 'merge',
+  cluster_by = ['block_timestamp::DATE']
+) }}
+
+WITH base AS (
+
+  SELECT
+    key,
+    VALUE,
+    event_id,
+    block_timestamp,
+    _INSERTED_TIMESTAMP
+  FROM
+    {{ ref('silver__set_mimir_events') }}
+
+{% if is_incremental() %}
+WHERE
+  _inserted_timestamp >= (
+    SELECT
+      MAX(
+        _inserted_timestamp
+      )
+    FROM
+      {{ this }}
+  )
+{% endif %}
+)
+SELECT
+  {{ dbt_utils.surrogate_key(
+    ['a.event_id','a.key','a.block_timestamp']
+  ) }} AS fact_set_mimir_events_id,
+  b.block_timestamp,
+  COALESCE(
+    b.dim_block_id,
+    '-1'
+  ) AS dim_block_id,
+  key,
+  VALUE,
+  A._INSERTED_TIMESTAMP,
+  '{{ env_var("DBT_CLOUD_RUN_ID", "manual") }}' AS _audit_run_id
+FROM
+  base A
+  LEFT JOIN {{ ref('core__dim_block') }}
+  b
+  ON A.block_timestamp = b.timestamp
